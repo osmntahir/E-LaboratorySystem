ee5d3ace94ae9375b4a0c18cb52d24e742d49b7e
Osman Tahir Özdemir <ozdemirosmantahir@gmail.com>
2024-12-30 14:10:01 +0300

    feat: Adjustments to bottom tab bar and keyboard compatibility

--- src/navigation/AdminNavigator.js
+++ src/navigation/AdminNavigator.js
@@ -2,149 +2,122 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import AdminHomeScreen from '../screens/admin/AdminHomeScreen';
-import GuideManagementScreen from '../screens/admin/GuideManagementScreen';
+// Alt bar’lı Tab Navigator
+import AdminTabNavigator from './AdminTabNavigator';
 
+// Tüm detay ekranları:
 import GuideDetailScreen from '../screens/admin/guideManagement/GuideDetailScreen';
 import TestDetailScreen from '../screens/admin/guideManagement/TestDetailScreen';
-
 import AddGuideScreen from '../screens/admin/guideManagement/AddGuideScreen';
 import EditGuideScreen from '../screens/admin/guideManagement/EditGuideScreen';
-
 import AddTestScreen from '../screens/admin/guideManagement/AddTestScreen';
 import EditTestScreen from '../screens/admin/guideManagement/EditTestScreen';
-
 import AddAgeGroupScreen from '../screens/admin/guideManagement/AddAgeGroupScreen';
 import EditAgeGroupScreen from '../screens/admin/guideManagement/EditAgeGroupScreen';
 
-import PatientListScreen from '../screens/admin/PatientListScreen';
 import PatientDetailScreen from '../screens/admin/PatientDetailScreen';
 import AddTestResultScreen from '../screens/admin/AddTestResultScreen';
 import EditTestResultScreen from '../screens/admin/EditTestResultScreen';
-
 import AddPatientScreen from '../screens/admin/AddPatientScreen';
 import UpdatePatientScreen from '../screens/admin/UpdatePatientScreen';
-
-import TestAnalysisScreen from '../screens/admin/TestAnalysisScreen';
-
 import PatientGraphScreen from '../screens/admin/PatientGraphScreen';
+
 
 const Stack = createStackNavigator();
 
 const AdminNavigator = () => {
-    return (
-        <Stack.Navigator initialRouteName="AdminHome">
-            <Stack.Screen
-                name="AdminHome"
-                component={AdminHomeScreen}
-                options={{ title: 'Admin Anasayfa' }}
-            />
+        return (
+            <Stack.Navigator>
+                    {/* 1) İlk ekranımız Tab Navigator olsun. */}
+                    <Stack.Screen
+                        name="MainTabs"
+                        component={AdminTabNavigator}
+                        options={{ headerShown: false }}
+                    />
 
-            {/** Kılavuz Yönetimi Anasayfa **/}
-            <Stack.Screen
-                name="GuideManagement"
-                component={GuideManagementScreen}
-                options={{ title: 'Kılavuz Yönetimi' }}
-            />
+                    {/* 2) Detay ekranları */}
+                    <Stack.Screen
+                        name="GuideDetail"
+                        component={GuideDetailScreen}
+                        options={{ title: 'Kılavuz Detayı' }}
+                    />
+                    <Stack.Screen
+                        name="TestDetail"
+                        component={TestDetailScreen}
+                        options={{ title: 'Test Detayı' }}
+                    />
 
-            {/** Hiyerarşik Ekranlar **/}
-            <Stack.Screen
-                name="GuideDetail"
-                component={GuideDetailScreen}
-                options={{ title: 'Kılavuz Detayı' }}
-            />
-            <Stack.Screen
-                name="TestDetail"
-                component={TestDetailScreen}
-                options={{ title: 'Test Detayı' }}
-            />
+                    <Stack.Screen
+                        name="AddGuide"
+                        component={AddGuideScreen}
+                        options={{ title: 'Kılavuz Ekle' }}
+                    />
+                    <Stack.Screen
+                        name="EditGuide"
+                        component={EditGuideScreen}
+                        options={{ title: 'Kılavuz Düzenle' }}
+                    />
+                    <Stack.Screen
+                        name="AddTest"
+                        component={AddTestScreen}
+                        options={{ title: 'Tetkik Ekle' }}
+                    />
+                    <Stack.Screen
+                        name="EditTest"
+                        component={EditTestScreen}
+                        options={{ title: 'Tetkik Düzenle' }}
+                    />
 
-            {/** Guide Add/Edit **/}
-            <Stack.Screen
-                name="AddGuide"
-                component={AddGuideScreen}
-                options={{ title: 'Kılavuz Ekle' }}
-            />
-            <Stack.Screen
-                name="EditGuide"
-                component={EditGuideScreen}
-                options={{ title: 'Kılavuz Düzenle' }}
-            />
+                    <Stack.Screen
+                        name="AddAgeGroup"
+                        component={AddAgeGroupScreen}
+                        options={{ title: 'Yaş Grubu Ekle' }}
+                    />
+                    <Stack.Screen
+                        name="EditAgeGroup"
+                        component={EditAgeGroupScreen}
+                        options={{ title: 'Yaş Grubu Düzenle' }}
+                    />
 
-            {/** Test Add/Edit **/}
-            <Stack.Screen
-                name="AddTest"
-                component={AddTestScreen}
-                options={{ title: 'Tetkik Ekle' }}
-            />
-            <Stack.Screen
-                name="EditTest"
-                component={EditTestScreen}
-                options={{ title: 'Tetkik Düzenle' }}
-            />
+                    <Stack.Screen
+                        name="PatientDetail"
+                        component={PatientDetailScreen}
+                        options={{ title: 'Hasta Detayı' }}
+                    />
+                    <Stack.Screen
+                        name="AddTestResult"
+                        component={AddTestResultScreen}
+                        options={{ title: 'Tahlil Ekle' }}
+                    />
+                    <Stack.Screen
+                        name="EditTestResult"
+                        component={EditTestResultScreen}
+                        options={{ title: 'Tahlil Düzenle' }}
+                    />
+                    {/* TestResultDetail için de ayrı bir screen isteniyorsa: */}
+                    {/* <Stack.Screen
+        name="TestResultDetail"
+        component={SomeDetailScreen} // varsa
+        options={{ title: 'Tahlil Detayı' }}
+      /> */}
 
-            {/** AgeGroup Add/Edit **/}
-            <Stack.Screen
-                name="AddAgeGroup"
-                component={AddAgeGroupScreen}
-                options={{ title: 'Yaş Grubu Ekle' }}
-            />
-            <Stack.Screen
-                name="EditAgeGroup"
-                component={EditAgeGroupScreen}
-                options={{ title: 'Yaş Grubu Düzenle' }}
-            />
-
-            {/** Hastalar vb. **/}
-            <Stack.Screen
-                name="Patients"
-                component={PatientListScreen}
-                options={{ title: 'Hasta Listesi' }}
-            />
-            <Stack.Screen
-                name="PatientDetail"
-                component={PatientDetailScreen}
-                options={{ title: 'Hasta Detayı' }}
-            />
-            <Stack.Screen
-                name="AddTestResult"
-                component={AddTestResultScreen}
-                options={{ title: 'Tahlil Ekle' }}
-            />
-            <Stack.Screen
-                name="EditTestResult"
-                component={EditTestResultScreen}
-                options={{ title: 'Tahlil Düzenle' }}
-            />
-            <Stack.Screen
-                name="TestResultDetail"
-                component={EditTestResultScreen}
-                options={{ title: 'Tahlil Detayı' }}
-            />
-
-            <Stack.Screen
-                name="TestAnalysis"
-                component={TestAnalysisScreen}
-                options={{ title: 'Hızlı Test Sorgu Ekranı' }}
-            />
-                <Stack.Screen
-                    name="AddPatient"
-                    component={AddPatientScreen}
-                    options={{ title: 'Hasta Ekle' }}
-                />
-                <Stack.Screen
-            name="UpdatePatient"
-            component={UpdatePatientScreen}
-            options={{ title: 'Hasta Güncelle' }}
-        />
-                <Stack.Screen
-                    name="PatientGraphic"
-                    component={PatientGraphScreen}
-                    options={{ title: 'Hasta Grafik' }}
-                />
-
-        </Stack.Navigator>
-    );
+                    <Stack.Screen
+                        name="AddPatient"
+                        component={AddPatientScreen}
+                        options={{ title: 'Hasta Ekle' }}
+                    />
+                    <Stack.Screen
+                        name="UpdatePatient"
+                        component={UpdatePatientScreen}
+                        options={{ title: 'Hasta Güncelle' }}
+                    />
+                    <Stack.Screen
+                        name="PatientGraphic"
+                        component={PatientGraphScreen}
+                        options={{ title: 'Hasta Grafiği' }}
+                    />
+            </Stack.Navigator>
+        );
 };
 
 export default AdminNavigator;
